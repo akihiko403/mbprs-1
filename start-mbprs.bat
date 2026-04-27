f6da6749336204e87720901b416d031c1f528924
lloyd <lloyddongon93@gmail.com>
2026-04-27 19:50:44 +0800

    added start.bat

--- start-mbprs.bat
+++ start-mbprs.bat
@@ -1,11 +1,6 @@
 @echo off
 setlocal
 
-if /i not "%~1"=="run-minimized" (
-    start "" /min cmd /c ""%~f0" run-minimized"
-    exit /b
-)
-
 cd /d "%~dp0"
 
 echo Starting Municipal Building Permit Repository System...
@@ -22,12 +17,4 @@ echo URL: http://127.0.0.1:8000
 echo Login: admin / password123
 echo.
 
-<<<<<<< HEAD
 endlocal
-=======
-start "" /min cmd /c ""%PHP_EXE%" artisan serve --host=127.0.0.1 --port=8000"
-timeout /t 2 /nobreak >nul
-start "" "http://127.0.0.1:8000/"
-
-endlocal
->>>>>>> 5a04e4c658c4983b2f9012602b8c4700d2230ef0
